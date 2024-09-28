fdf2d87c2fbd5684e174c17dd068ad2eaa3a4bec
azazeldev <68147363+azazelcodes@users.noreply.github.com>
2024-09-28 12:56:40 +0200

    [Fixes] Unclutter build and annotate mod
    now its runnable! (hopefully)

--- src/main/java/me/azazeldev/gndfiles/Main.java
+++ src/main/java/me/azazeldev/gndfiles/Main.java
@@ -6,18 +6,20 @@ import me.azazeldev.gndfiles.gndmain.types.Node;
 import me.azazeldev.gndfiles.gndmain.Parser;
 import me.azazeldev.gndfiles.gui.Command;
 import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ProgressManager;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@Mod(modid = "", useMetadata = true)
 public class Main {
     public static Main instance;
     public static Command commandManager = new Command();
     public static Logger l;
     public static Map<String, Node> nodeMap;
 
-    @net.minecraftforge.fml.common.Mod.EventHandler
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event) throws IOException {
         ProgressManager.ProgressBar progressBar = ProgressManager.push("gnd", 2);
         instance = this;
